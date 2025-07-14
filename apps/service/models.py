2ed96156e52f13f5020e353d16a1985b3bfa22df
Bibek Gautam <beebayk0001@gmail.com>
2025-07-14 12:27:28 +0000

    feat: related models for services

--- apps/service/models.py
+++ apps/service/models.py
@@ -1,3 +1,57 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration = models.IntegerField()
+    image = models.ImageField(upload_to="services/")
+
+    def __str__(self):
+        return self.name
+
+
+class Staff(models.Model):
+
+    ROLES = [("beautician", "Beautician"), ("dermatologist", "Dermatologist")]
+
+    name = models.CharField(max_length=100)
+    role = models.CharField(max_length=100, choices=ROLES, default="beautician")
+    bio = models.TextField()
+    image = models.ImageField(upload_to="staff/")
+    services = models.ManyToManyField(Service, related_name="staff")
+
+    def __str__(self):
+        return self.name
+
+
+class Appointment(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    date = models.DateField()
+    time = models.TimeField()
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        related_name="appointments",
+        null=True,
+        blank=True,
+    )
+    service = models.ForeignKey(
+        Service, on_delete=models.CASCADE, related_name="appointments"
+    )
+    staff = models.ForeignKey(
+        Staff,
+        on_delete=models.SET_NULL,
+        related_name="appointments",
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return f"{self.name} - {self.service.name} - {self.date} - {self.time}"
