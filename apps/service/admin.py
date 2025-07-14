c69c3553372d35d41ee85112c694193f250bdcda
Bibek Gautam <beebayk0001@gmail.com>
2025-07-14 12:28:56 +0000

    feat: admin panel for services"

--- apps/service/admin.py
+++ apps/service/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Service, Staff, Appointment
 
-# Register your models here.
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("name", "price")
+    search_fields = ("name",)
+
+
+@admin.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ("name", "role")
+    search_fields = ("name",)
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "phone", "date", "time", "service", "staff")
+    search_fields = ("name", "email", "phone")
+    list_filter = ("date", "service", "staff")
